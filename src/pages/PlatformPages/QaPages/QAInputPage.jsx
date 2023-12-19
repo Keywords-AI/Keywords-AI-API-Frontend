3fc91cb170c28d3419a17e6a542d28c44d85e61a
YunhaoLiu1228 <yunhaol4@illinois.edu>
2023-12-18 21:59:29 -0800

    add untracked

---
+++ src/pages/PlatformPages/QaPages/QAInputPage.jsx
@@ -0,0 +1,18 @@
+import react from "react";
+import { NavBar, PageContent, PageParagraph } from 'src/components/Sections';
+
+
+export const QAInputPage = () => {
+    return (
+        <PageContent title="Input Field Wall">
+            <PageParagraph heading="Nav Bar">
+            </PageParagraph>
+
+            <PageParagraph heading="">
+            
+            </PageParagraph>
+        </PageContent>
+    )
+
+
+}
