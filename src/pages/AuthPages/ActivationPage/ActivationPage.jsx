2b76fcb432db29eb566bbbc0d5e0a0ce01e9be4f
Raymond1415926 <1415176717@qq.com>
2023-12-29 01:01:39 -0800

    update test endponts

--- src/pages/AuthPages/ActivationPage/ActivationPage.jsx
+++ src/pages/AuthPages/ActivationPage/ActivationPage.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { BackButton, Button } from 'src/components/Buttons'
 import { TitleAuth } from 'src/components/Titles'
 import { useForm } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
-import usePost from 'src/hooks/usePost'
+import { activateUser } from 'src/store/actions'
 
 const mapStateToProps = (state) => ({})
 
 const mapDispatchToProps = {}
 
 export const ActivationPage = (props) => {
-  const { loading, error, data, postData } = usePost({
-    path: "auth/users/activation/"
-  });
+  const [countDown, setCountDown] = React.useState(5);
   const { uid, token } = useParams();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-
   const onSubmit = async (data) => {
-    console.log(uid, token)
-    // postData({ uid, token })
+    activateUser(uid, token);
   };
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      if (countDown===0) return;
+      setCountDown(countDown-1)
+    }, 1000)
+    return ()=>clearInterval(interval)
+  },[countDown])
+  useEffect(()=>{
+    activateUser(uid, token);
+  },[])
   return (
     <div className="flex-col items-center gap-xxxl justify-center self-stretch">
       <div className="flex-col items-start gap-[10px] self-stretch">
@@ -32,8 +38,8 @@ export const ActivationPage = (props) => {
       </div>
       <div className=" flex-col w-full max-w-[420px] items-center gap-lg justify-center ">
         <TitleAuth
-          title={"Activate your account"}
-          subtitle={`Click the button below to confirm your activation for your account`}
+          title={"Activating your account..."}
+          subtitle={`Click the button below if you did not get automatically  redirected in ${countDown} seconds`}
         />
         <form
           onSubmit={handleSubmit(onSubmit)}
--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,7 +1,7 @@
 import { retrieveAccessToken } from "src/utilities/authorization";
 
 const apiConfig = {
-  // apiURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'? 'https://platform-test.keywordsai.co/' : 'https://platform-test.keywordsai.co/',
+  // apiURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'? 'https://api-test.keywordsai.co/' : 'https://api-test.keywordsai.co/',
   apiURL: "http://localhost:8000/",
   frontendURL: window.location.origin,
   apiKey: "your-api-key",
