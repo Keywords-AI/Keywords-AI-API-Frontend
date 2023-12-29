1615c0fab61c5a0283717977293425cbd8eb0339
YunhaoLiu1228 <yunhaol4@illinois.edu>
2023-12-29 02:50:55 -0800

    updated dashboard

--- src/pages/AuthPages/Dashboard/Dashboard.jsx
+++ src/pages/AuthPages/Dashboard/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { ButtonGroup } from "src/components/Buttons";
+// import { ButtonGroup } from "src/components/Buttons";
 import MetricCard from "src/components/Cards/MetricCard";
 import { PageContent } from "src/components/Sections";
-import { Cost } from "../Playground/components/ModelOutput/icons";
-import { Quality, Rocket } from "src/components";
-import { Speed } from "../Playground/components/OptionSelector/icons";
+import { Quality, Rocket } from "src/components/Icons";
+import { Cost, Tokens } from "src/pages/PlatformPages/Playground/components/ModelOutput/icons";
+import { Speed } from "src/pages/PlatformPages/Playground/components/OptionSelector/icons";
+import { Button } from "src/components";
+import ButtonGroup from "src/components/Buttons/ButtonGroup";
+import { UsageChart } from "src/components/Display";
+import { TitleStaticSubheading } from "src/components/Titles";
 
 const buttons = [
   { text: "Day", onClick: () => console.log("Clicked Day") },
@@ -13,36 +17,41 @@ const buttons = [
   { text: "Quarter", onClick: () => console.log("Clicked Quarter") },
 ];
 
-export default function Dashboard() {
+export function Dashboard() {
   return (
     <div className="flex flex-wrap flex-col w-full h-full p-lg gap-lg">
-      <div className="flex flex-row justify-between w-full">
+      <div className="flex flex-row justify-between w-full self-stretch">
         <span className="display-sm">Dashboard</span>
-        <ButtonGroup buttons={buttons}></ButtonGroup>
+        <ButtonGroup buttons={buttons}/>
       </div>
-      <div className="flex flex-row gap-md h-fit">
+      <div className="grid grid-cols-4 gap-md">
         <MetricCard 
             icon={Cost} 
-            title="Total cost" 
+            title="Cost" 
             number="$0.29"/>
         <MetricCard 
             icon={Rocket} 
-            title="Total request" 
+            title="Request" 
             number="82" 
             percentage="130"/>
         <MetricCard
-          icon={Quality}
-          title="Average cost / request"
-          number="$0.0002"
+          icon={Tokens}
+          title="Tokens"
+          number="134057"
           percentage="29"
+          fill="fill-error"
+          text="text-error"
         />
         <MetricCard
           icon={Speed}
-          title="Average cost / request"
+          title="Average latency / request"
           number="9.9ms"
           percentage="12"
+          up={false}
         />
       </div>
+      <UsageChart dataKeyX={"name"} dataKeyY="usage"/>
+      <TitleStaticSubheading title="Log" subtitle="Quality of the model output."/>
     </div>
   );
 }
