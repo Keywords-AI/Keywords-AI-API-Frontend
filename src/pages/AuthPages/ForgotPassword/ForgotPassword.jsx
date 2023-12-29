43748f89241a9361a0e0730cc0a76917b1f499a7
Raymond1415926 <1415176717@qq.com>
2023-12-29 02:09:48 -0800

    auth basically completed

--- src/pages/AuthPages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/AuthPages/ForgotPassword/ForgotPassword.jsx
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { BackButton, Button } from "src/components/Buttons";
 import { AuthenticationTitle } from "src/components/Titles";
-import cn from "src/utilities/classMerge";
 import { TextInput } from "src/components/Inputs";
-import usePost from "src/hooks/usePost";
+import { connect } from "react-redux";
+import { resetPassword } from "src/store/actions";
 
-export function ForgotPassword() {
-  const { loading, error, data, postData } = usePost({path: "auth/users/reset-password/"});
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = { resetPassword };
+
+function ForgotPasswordNotConnected({ resetPassword }) {
   const navigate = useNavigate();
   const {
     register,
@@ -17,29 +19,29 @@ export function ForgotPassword() {
   const emailError = errors.email;
   const onSubmit = (formData) => {
     console.log(formData);
-    postData(formData);
+    resetPassword(formData.email);
   }
 
   return (
     <div className="flex-col items-center gap-xxxl justify-center self-stretch">
       <div className="flex flex-col items-start self-stretch gap-[10px]">
-        <BackButton text="Home" link="/"/>
+        <BackButton text="Home" link="/" />
       </div>
       <form className="flex flex-col w-full max-w-[420px] items-center gap-lg justify-center"
-      onSubmit={handleSubmit(onSubmit)}
-      > 
-        <AuthenticationTitle title="Forgot password" subtitle="You will receive a link to reset your password."/>
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <AuthenticationTitle title="Forgot password" subtitle="You will receive a link to reset your password." />
         <div className="flex-col justify-center items-center gap-[20px] self-stretch">
           <div className="flex-col justify-center items-start gap-xs self-stretch">
-            <TextInput {...register("email")} type="email" title="Email" placeholder="Put your email here"/>
+            <TextInput {...register("email")} type="email" title="Email" placeholder="Put your email here" />
           </div>
           <div className="flex-col justify-center items-center gap-xs self-stretch">
-            <Button variant="r4-white" text="Send reset link" className="min-w-[60px] self-stretch items-center justify-center gap-xxs"/>
-            {/* <span className="text-error caption self-stretch">We cannot find your email.</span> */}
-            {data && !error && <span className="text-success caption self-stretch">Link sent to your email inbox via thekeywordsai@gmail.com. </span>}
+            <Button variant="r4-white" text="Send reset link" className="min-w-[60px] self-stretch items-center justify-center gap-xxs" />
           </div>
         </div>
       </form>
     </div>
   );
 }
+
+export const ForgotPassword = connect(mapStateToProps, mapDispatchToProps)(ForgotPasswordNotConnected);
--- src/pages/AuthPages/ResetPassword/ResetPassword.jsx
+++ src/pages/AuthPages/ResetPassword/ResetPassword.jsx
@@ -1,17 +1,22 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { BackButton, Button } from "src/components/Buttons";
 import { AuthenticationTitle } from "src/components/Titles";
-import cn from "src/utilities/classMerge";
 import { TextInput } from "src/components/Inputs";
-import usePost from "src/hooks/usePost";
+import { resetPasswordConfirm } from "src/store/actions";
+import { connect } from "react-redux";
+import React from "react";
 
-export function ResetPassword() {
-  const { loading, error, data, postData } = usePost({
-    path: "auth/users/reset-password/",
-    auth: false,
-  });
+
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = {
+  resetPasswordConfirm,
+};
+
+
+function ResetPasswordNotConnected({ resetPasswordConfirm }) {
   const navigate = useNavigate();
+  const { uid, token } = useParams();
   const {
     register,
     handleSubmit,
@@ -20,23 +25,23 @@ export function ResetPassword() {
   const emailError = errors.email;
   const onSubmit = (formData) => {
     console.log(formData);
-    postData(formData);
+    resetPasswordConfirm({uid, token, ...formData})
   }
   return (
     <div className="flex-col items-center gap-xxxl justify-center self-stretch">
       <div className="flex flex-col items-start self-stretch gap-[10px]">
-        <BackButton text="Home" link="/"/>
+        <BackButton text="Home" link="/" />
       </div>
       <form className="flex flex-col w-full max-w-[420px] items-center gap-lg justify-center"
-      onSubmit={handleSubmit(onSubmit)}>
-        <AuthenticationTitle title="Reset password" subtitle="Reset your password below."/>
+        onSubmit={handleSubmit(onSubmit)}>
+        <AuthenticationTitle title="Reset password" subtitle="Reset your password below." />
         <div className="flex-col justify-center items-center gap-[20px] self-stretch">
           <div className="flex-col justify-center items-start gap-xs self-stretch">
-            <TextInput title="New password" type="password" placeholder=""/>
-            <TextInput title="Confirm password" placeholder=""/>
+            <TextInput {...register("new_password")} title="New password" type="password" placeholder="" />
+            <TextInput {...register("re_new_password")} title="Confirm password" type="password" placeholder="" />
           </div>
           <div className="flex-col justify-center items-center gap-xs self-stretch">
-            <Button variant="r4-white" text="Change password" className="min-w-[60px] self-stretch items-center justify-center gap-xxs"/>
+            <Button variant="r4-white" text="Change password" className="min-w-[60px] self-stretch items-center justify-center gap-xxs" />
             {false && <span className="text-error caption self-stretch">Passwords do not match.</span>}
           </div>
         </div>
@@ -44,3 +49,8 @@ export function ResetPassword() {
     </div>
   );
 }
+
+export const ResetPassword = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ResetPasswordNotConnected);
