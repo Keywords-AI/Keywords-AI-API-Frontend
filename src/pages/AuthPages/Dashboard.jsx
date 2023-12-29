d7895b131cf157cfd6af3d98cbf165811d5f8d0e
Andydy25 <hanheli25@gmail.com>
2023-12-29 02:56:49 -0800

    swap buttons

---
+++ src/pages/AuthPages/Dashboard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { ButtonGroup } from "src/components/Buttons";
+import MetricCard from "src/components/Cards/MetricCard";
+import { PageContent } from "src/components/Sections";
+
+const buttons = [
+  { text: "Day", onClick: () => console.log("Clicked Day") },
+  { text: "Week", onClick: () => console.log("Clicked Week") },
+  { text: "Month", onClick: () => console.log("Clicked Month") },
+  { text: "Quarter", onClick: () => console.log("Clicked Quarter") },
+];
+
+export default function Dashboard() {
+  return (
+    <div className="flex flex-wrap flex-col w-full p-lg gap-lg">
+      <div className="flex flex-row justify-between w-full">
+        <span className="display-sm">Dashboard</span>
+        <ButtonGroup buttons={buttons}></ButtonGroup>
+      </div>
+      <div className="flex p-0 h-fit">
+        <MetricCard 
+           // icon={Cost} 
+            title="Total cost" 
+            number="$0.29"/>
+        <MetricCard 
+            //icon={Rocket} 
+            title="Total request" 
+            number="82" 
+            percentage="130"/>
+        <MetricCard
+         // icon={Quality}
+          title="Average cost / request"
+          number="$0.0002"
+          percentage="29"
+        />
+        <MetricCard
+        //  icon={Speed}
+          title="Average cost / request"
+          number="9.9ms"
+          percentage="12"
+        />
+      </div>
+    </div>
+  );
+}
