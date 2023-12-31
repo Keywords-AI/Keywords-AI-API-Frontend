80f3c219abd04cac1175d2f2293a726f5d54f845
failury <failury29@gmail.com>
2023-12-30 19:12:46 -0500

    Update user settings page and fix isLoggedIn bug

--- src/pages/PlatformPages/SettingPages/SettingPages.jsx
+++ src/pages/PlatformPages/SettingPages/SettingPages.jsx
@@ -1,83 +1,81 @@
-import React, { useEffect } from 'react'
-import { textToLink } from 'src/utilities/stringProcessing';
-import SettingsPage from './SettingsPage';
-import ApiKeyPage from './ApiKeyPage';
-import UsagePage from './UsagePage';
-import MemberPage from './MemberPage';
-import BillingPage from './BillingPage';
-import { Building } from 'src/components/Icons'
-import { IntegrationsPage } from './IntegrationsPage';
-import { generateChild } from 'src/utilities/objectProcessing';
+import React, { useEffect } from "react";
+import { textToLink } from "src/utilities/stringProcessing";
+import SettingsPage from "./SettingsPage";
+import ApiKeyPage from "./ApiKeyPage";
+import UsagePage from "./UsagePage";
+import MemberPage from "./MemberPage";
+import BillingPage from "./BillingPage";
+import { Building } from "src/components/Icons";
+import { IntegrationsPage } from "./IntegrationsPage";
+import { generateChild } from "src/utilities/objectProcessing";
+import UserSettings from "./UserSettings";
 
 const pages = [
-    {
-        title: "Settings",
-        // forAdmin: true,
-        default: true,
-        page: <SettingsPage />,
-    },
-    {
-        title: "Settings",
-        page: <SettingsPage />,
-    },
-    {
-        title: "Usage",
-        // forAdmin: true,
-        page: <UsagePage />,
-    },
-    {
-        title: "Member",
-        forAdmin: true,
-        page: <MemberPage />,
-    },
-    {
-        title: "Billing",
-        // forAdmin: true,
-        page: <BillingPage />,
-    },
+  {
+    title: "Settings",
+    // forAdmin: true,
+    default: true,
+    page: <SettingsPage />,
+  },
 
-    {
-        title: "Integrations",
-        // forAdmin: true,
-        page: <IntegrationsPage />,
-    },
+  {
+    title: "Usage",
+    // forAdmin: true,
+    page: <UsagePage />,
+  },
+  {
+    title: "Member",
+    forAdmin: true,
+    page: <MemberPage />,
+  },
+  {
+    title: "Billing",
+    // forAdmin: true,
+    page: <BillingPage />,
+  },
 
+  {
+    title: "Integrations",
+    // forAdmin: true,
+    page: <IntegrationsPage />,
+  },
 ];
 
-
-
 const userPages = [
+  {
+    title: "Settings",
+    page: <UserSettings />,
+  },
 
-    {
-        title: "API Keys",
-        // forAdmin: true,
-        page: <ApiKeyPage />,
-    },
-]
-
+  {
+    title: "API Keys",
+    // forAdmin: true,
+    page: <ApiKeyPage />,
+  },
+];
 
 const processedOrgPages = pages.map((page, index) => {
-    return generateChild(page);
-})
+  return generateChild(page);
+});
 
 const processedUserPages = userPages.map((page, index) => {
-    return generateChild(page);
-})
+  return generateChild(page);
+});
 
 export const sections = [
-    {
-        title: "Organization",
-        pages: processedOrgPages,
-        icon: <Building />
-    },
-    {
-        title: "User",
-        pages: processedUserPages
-    }
-]
+  {
+    title: "Organization",
+    pages: processedOrgPages,
+    icon: <Building />,
+  },
+  {
+    title: "User",
+    pages: processedUserPages,
+  },
+];
 
 export const settingChildren = sections.reduce((allPages, section) => {
-    const newPages = section.pages;
+  const newPages = section.pages;
 
-    return [...allPages, ...newPages];
+  return [...allPages, ...newPages];
 }, []);
