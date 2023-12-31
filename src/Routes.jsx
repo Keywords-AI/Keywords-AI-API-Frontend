6a9d572e039c2f369f6fbe7481a2e651ccff5b7d
Raymond1415926 <1415176717@qq.com>
2023-12-31 02:30:09 -0800

    update auth

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -50,8 +50,8 @@ const Routes = ({ getUser, user }) => {
     }, 1000 * 10 * 60);
     return () => clearInterval(intervalId);
   }, [authToken]);
-  // const isUserLoggedIn = isLoggedIn(user);
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = isLoggedIn(user);
+  // const isUserLoggedIn = true;
   const routes = [
     {
       path: "/platform",
