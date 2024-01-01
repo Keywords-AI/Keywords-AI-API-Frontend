180724e28420acb7fd40d1bca98ecfcf6fc33d9c
YunhaoLiu1228 <yunhaol4@illinois.edu>
2023-12-31 18:15:40 -0800

    Integrate modelpresetCards

--- src/store/actions/dashboardAction.js
+++ src/store/actions/dashboardAction.js
@@ -1,3 +1,5 @@
+import { data } from "autoprefixer";
+import moment from 'moment';
 import { keywordsFetch } from "src/services/apiConfig";
 import { sliceChartData } from "src/utilities/objectProcessing";
 export const GET_DASHBOARD_DATA = "GET_DASHBOARD_DATA";
@@ -6,6 +8,38 @@ export const SET_COST_DATA = "SET_COST_DATA";
 export const SET_TOKEN_COUNT_DATA = "SET_TOKEN_COUNT_DATA";
 export const SET_LATENCY_DATA = "SET_LATENCY_DATA";
 export const SET_REQUEST_COUNT_DATA = "SET_REQUEST_COUNT_DATA";
+export const SET_DATE_DATA = "SET_DATE_DATA";
+
+export const aggregateData = (data, timePeriod) => {
+  const formattedData = [];
+  data.forEach(item => {
+    
+    switch (timePeriod) {
+      case 'daily':
+        
+        break;
+      case 'weekly':
+        
+        break;
+      case 'monthly':
+        
+        break;
+      case 'yearly':
+        // dateGroup = date.startOf('year').format('YYYY-MM-DD');
+        break;
+      default:
+        dateGroup = date.format('YYYY-MM-DD');
+    }
+
+    if (!groupedData[dateGroup]) {
+      // groupedData[dateGroup] = { ...item, [dateKey]: dateGroup, [valueKey]: 0 };
+    }
+    // groupedData[dateGroup][valueKey] += item[valueKey];
+  });
+
+  return Object.values(groupedData);
+};
+
 
 export const setDashboardData = (data) => {
   return {
@@ -42,6 +76,12 @@ export const setRequestCountData = (data) => {
   };
 }
 
+export const setDateData = (data) => {
+  return {
+    type: SET_DATE_DATA,
+    payload: data, 
+  };
+}
 
 export const getDashboardData = () => {
   return (dispatch) => {
@@ -54,7 +94,9 @@ export const getDashboardData = () => {
         if (!response.ok) {
           throw new Error("Error");
         } else {
+          console.log(response.json());
           return response.json();
+          
         }
       })
       .then((data) => {
@@ -70,3 +112,5 @@ export const getDashboardData = () => {
       });
   };
 };
+
+
