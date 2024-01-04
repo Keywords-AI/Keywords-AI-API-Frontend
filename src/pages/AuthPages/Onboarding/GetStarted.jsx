bdb53b0c78c3579619064dc0be110cae4c12797a
Andydy25 <hanheli25@gmail.com>
2024-01-04 15:01:47 -0800

    build get started page

--- src/pages/AuthPages/Onboarding/GetStarted.jsx
+++ src/pages/AuthPages/Onboarding/GetStarted.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from "react";
 import { Button } from "src/components";
 import cn from "src/utilities/classMerge";
@@ -9,24 +8,31 @@ import { REDIRECT_URI } from "src/utilities/navigation";
 
 const mapStateToProps = (state) => ({});
 const mapDispatchToProps = { updateUser };
-export const GetStarted = connect(mapStateToProps, mapDispatchToProps)(({
-    show = false,
-    updateUser
-}) => {
-    const navigate = useNavigate();
-    return (
-        <div className={
-            cn("flex-col self-stretch flex-grow justify-center",
-                show ? "visible" : "invisible"
-            )
-        }
-        >
-            <Button variant="r4-primary" text="Get started"
-                onClick={() => {
-                    updateUser({ onboarded: true });
-                    navigate(REDIRECT_URI);
-                }}
-            />
-        </div>
-    );
-})
+export const GetStarted = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(({ show = false, updateUser }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className={cn(
+        "flex-col self-stretch flex-grow justify-center gap-sm items-center",
+        show ? "visible" : "invisible"
+      )}
+    >
+      <TitleAuth
+        title="Setup Complete!"
+        subtitle={"Kickstart your journey to smarter LLM usage with Keywords AI."}
+        align="items-center"
+      />
+      <Button
+        variant="r4-primary"
+        text="Get started"
+        onClick={() => {
+          updateUser({ onboarded: true });
+          navigate(REDIRECT_URI);
+        }}
+      />
+    </div>
+  );
+});
