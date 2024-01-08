cb2dd91b7534927d46c7e86e1eea5702e20d16c5
failury <failury29@gmail.com>
2024-01-08 00:31:42 -0500

    Add Payment component to Routes.jsx

--- src/pages/PlatformPages/Payment.jsx
+++ src/pages/PlatformPages/Payment.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 import { Button } from "src/components/Buttons";
-import { createPaymentSession } from 'src/services/stripe';
-const lookupKeys = import.meta.env.VITE_PRODUCT_LOOKUP_KEYS.split(',')
+import { createPaymentSession } from "src/services/stripe";
+const lookupKeys = import.meta.env.VITE_PRODUCT_LOOKUP_KEYS.split(",");
 
+const mapStateToProps = (state) => ({});
 
-const mapStateToProps = (state) => ({})
-
-const mapDispatchToProps = {}
+const mapDispatchToProps = {};
 
 export const Payment = (props) => {
-    const handlePayment = () => {
-        console.log("Payment", lookupKeys);
-        createPaymentSession(lookupKeys)
-    }
-    return (
-        <div className="flex-col flex-grow self-stretch items-center justify-center">
-            <Button
-                variant="r4-primary"
-                type="button"
-                text="Pay your bill"
-                onClick={handlePayment}
-            />
-        </div>
-    )
-}
+  const handlePayment = () => {
+    console.log("Payment", lookupKeys);
+    createPaymentSession(lookupKeys);
+  };
+  return (
+    <div className="flex-col flex-grow self-stretch items-center justify-center">
+      <Button
+        variant="r4-primary"
+        type="button"
+        text="Pay your bill"
+        onClick={handlePayment}
+      />
+    </div>
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Payment)
+export default connect(mapStateToProps, mapDispatchToProps)(Payment);
