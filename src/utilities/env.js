e954db8de90fef07f5e92d57aeffecb96143f456
Raymond1415926 <1415176717@qq.com>
2024-01-10 13:03:27 -0800

    prod

--- src/utilities/env.js
+++ src/utilities/env.js
@@ -1,4 +1,4 @@
-export const FETCH_ENDPOINT=import.meta.env.VITE_FETCH_ENDPOINT || "https://api.keywordsai.co/";
+export const FETCH_ENDPOINT="https://api.keywordsai.co/";
 export const AUTH_ENABLED=import.meta.env.VITE_AUTH_ENABLED || "true";
 export const STRIPE_STATER_LOOKUP_KEY=import.meta.env.VITE_STRIPE_STATER_LOOKUP_KEY || "keywords_ai_api_starter_plan";
 export const STRIPE_TEAM_LOOKUP_KEY=import.meta.env.VITE_STRIPE_TEAM_LOOKUP_KEY || "keywords_ai_api_team_plan";
