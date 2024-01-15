6d4dd42ed1068b716cdc918aea3c398bac215408
YunhaoLiu1228 <yunhaol4@illinois.edu>
2024-01-15 10:09:05 -0800

    update dashboard

--- src/pages/PlatformPages/Dashboard/Dashboard.jsx
+++ src/pages/PlatformPages/Dashboard/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
 // import { ButtonGroup } from "src/components/Buttons";
 import MetricCard from "src/components/Cards/MetricCard";
-import { Quality, Rocket, Cost, Tokens, Speed } from "src/components/Icons";
+import {
+  Quality,
+  Rocket,
+  Cost,
+  Tokens,
+  Speed,
+  Display,
+  Down,
+  sideBar,
+} from "src/components/Icons";
 import ButtonGroup from "src/components/Buttons/ButtonGroup";
 import { setQueryParams } from "src/utilities/navigation";
 import { TitleAuth, TitleStaticSubheading } from "src/components/Titles";
@@ -11,6 +20,8 @@ import { getDashboardData, setDateData, setPanelData } from "src/store/actions";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Button } from "src/components";
 import { PanelGraph } from "src/components/Sections";
+import { SelectInput } from "src/components/Inputs";
+import { DotsButton } from "src/components/Buttons";
 
 const mapStateToProps = (state) => ({
   summary: state.dashboard.summary,
@@ -22,6 +33,8 @@ const mapStateToProps = (state) => ({
   costData: state.dashboard.costData,
   firstTime: !state.organization?.has_api_call,
   panelData: state.dashboard.panelData,
+  promptTokenCountData: state.dashboard.promptTokenCountData,
+  completionTokenCountData: state.dashboard.completionTokenCountData,
 });
 const mapDispatchToProps = {
   getDashboardData,
@@ -63,6 +76,8 @@ function DashboardNotConnected({
   requestCountData,
   latencyData,
   tokenCountData,
+  promptTokenCountData,
+  completionTokenCountData,
   costData,
   getDashboardData,
   firstTime,
@@ -71,7 +86,7 @@ function DashboardNotConnected({
 }) {
   const navigate = useNavigate();
   const location = useLocation();
-  const summary_type = new URLSearchParams(location.search).get("summary_type");
+  const summary_type = new URLSearchParams(location.search).get("summary_type") || "monthly";
   const [isPanel, setIsPanel] = useState(false);
   useEffect(() => {
     getDashboardData();
@@ -83,60 +98,58 @@ function DashboardNotConnected({
 
   const handleClick = (data) => {
     setPanelData(data);
-    setIsPanel(true);
+    setIsPanel(prevIsPanel => !prevIsPanel);
   };
 
+  const handleTimePeriodSelection = (selectedValue) => {
+    setSummaryType(selectedValue);
+  };
   const setSummaryType = (summary_type) => {
     // Wrapper, for cleaner code
     setQueryParams({ summary_type }, navigate);
   };
-  const buttons = [
-    { text: "Day", onClick: () => setSummaryType("daily") },
-    { text: "Week", onClick: () => setSummaryType("weekly") },
-    { text: "Month", onClick: () => setSummaryType("monthly") },
-    { text: "Year", onClick: () => setSummaryType("yearly") },
-  ];
+  // const buttons = [
+  //   { text: "Day", onClick: () => setSummaryType("daily") },
+  //   { text: "Week", onClick: () => setSummaryType("weekly") },
+  //   { text: "Month", onClick: () => setSummaryType("monthly") },
+  //   { text: "Year", onClick: () => setSummaryType("yearly") },
+  // ];
   const metrics = [
     {
-      icon: Rocket,
       title: "Request",
       number: summary.number_of_requests,
       chartData: requestCountData,
       dataKey: "number_of_requests",
-      onClick: () => handleClick("Request"),
+      // onClick: () => handleClick("Request"),
     },
     {
-      icon: Speed,
       title: "Latency",
       number: `${summary.average_latency?.toFixed(3) || 0}`,
       chartData: latencyData,
       dataKey: "average_latency",
       unit: true,
-      onClick: () => handleClick("Latency"),
+      // onClick: () => handleClick("Latency"),
     },
     {
-      icon: Tokens,
       title: "Prompt Tokens",
       number: summary.total_prompt_tokens || 0,
-      chartData: tokenCountData,
+      chartData: promptTokenCountData,
       dataKey: "total_prompt_tokens",
-      onClick: () => handleClick("Tokens"),
+      // onClick: () => handleClick("Tokens"),
     },
     {
-      icon: Tokens,
       title: "Output Tokens",
       number: summary.total_completion_tokens || 0,
-      chartData: tokenCountData,
+      chartData: completionTokenCountData,
       dataKey: "total_completion_tokens",
-      onClick: () => handleClick("Tokens"),
+      // onClick: () => handleClick("Tokens"),
     },
     {
-      icon: Cost,
       title: "Cost",
       number: `$${summary.total_cost?.toFixed(3) || 0}`,
       chartData: costData,
       dataKey: "total_cost",
-      onClick: () => handleClick("Cost"),
+      // onClick: () => handleClick("Cost"),
     },
   ];
   const MetricNumber = (panelData) =>
@@ -144,24 +157,58 @@ function DashboardNotConnected({
   if (firstTime !== undefined && firstTime) return <WelcomeState />;
   else
     return (
-        <div className="flex flex-col w-full h-full">
-          <div className="flex flex-row justify-between w-full self-stretch">
-            <div className="flex flex-col gap-xxxs">
-              {/* <span className="text-sm-regular text-gray-3">
+      <div className="flex flex-col w-full h-full">
+        {/* <span className="text-sm-regular text-gray-3">
                 {organization?.name || "Organization"}
               </span> */}
-              <span className="display-sm">Welcome, {firstName}</span>
-            </div>
-            {/* <ButtonGroup buttons={buttons} /> */}
+
+        {/* <ButtonGroup buttons={buttons} /> */}
+        <div className="grid grid-cols-6 px-lg items-start self-stretch">
+          <div className="flex flex-col py-md items-start gap-xxs self-stretch">
+            <span className="text-sm-md text-gray-4">Welcome</span>
+            <span className="display-sm text-gray-5">{firstName} </span>
           </div>
-          <div className="grid grid-cols-5 gap-xs">
-            {metrics.map((metric, index) => (
-              <MetricCard key={index} {...metric} />
-            ))}
-          </div>
-            <DashboardChart />
-            {/* <PanelGraph metric={panelData} number={MetricNumber(panelData)} /> */}
+          {metrics.map((metric, index) => (
+            <MetricCard key={index} {...metric} />
+          ))}
         </div>
+        <div className="flex flex-row py-xs px-lg justify-between items-center self-stretch shadow-border shadow-gray-2 w-full">
+          <div className="flex items-center content-center gap-xs flex-wrap">
+            {" "}
+          </div>
+          <div className="flex items-center gap-xxs">
+            <Button variant="small" text="Today" />
+            <SelectInput
+              headLess
+              placeholder="Month"
+              align="start"
+              icon={Down}
+              padding="py-xxxs px-xs"
+              gap="gap-xxs"
+              choices={[
+                { name: "Day", value: "daily" },
+                { name: "Week", value: "weekly" },
+                { name: "Month", value: "monthly" },
+                { name: "Year", value: "yearly" },
+              ]}
+              handleSelected={handleTimePeriodSelection}
+            />
+            <Button
+              variant="small"
+              text="Display"
+              icon={Display}
+              secIcon={Down}
+              secIconPosition="right"
+            />
+            <div className="w-[1px] h-[28px] shadow-border shadow-gray-2 "></div>
+            <DotsButton icon={sideBar} bgColor="bg-gray-2" onClick={() => handleClick("Request")}/>
+          </div>
+        </div>
+        <div className="flex flex-row">
+        <DashboardChart />
+        {isPanel && <PanelGraph metric={panelData} number={MetricNumber(panelData)} />}
+        </div>
+      </div>
     );
 }
 
