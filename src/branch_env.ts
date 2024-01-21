758842e2fcbf654f07f95881d992ceb3a05d1638
Raymond1415926 <1415176717@qq.com>
2024-01-21 09:02:18 -0800

    branch_env

--- src/branch_env.ts
+++ src/branch_env.ts
@@ -1,4 +1,10 @@
 export const envVars = {
-    FETCH_ENDPOINT: "http://localhost:8000/",
-    AUTH_ENABLED: "true", // keep it string, so that it is also compatible with .env setups
+    FETCH_ENDPOINT: "https://api.keywordsai.co/",
+    AUTH_ENABLED: "true",
+    STRIPE_STATER_LOOKUP_KEY: "keywords_ai_api_starter_plan",
+    STRIPE_TEAM_MONTHLY_FLAT_LOOKUP_KEY: "keywords_ai_api_team_plan_monthly_flat",
+    STRIPE_TEAM_MONTHLY_USAGE_LOOKUP_KEY: "keywords_ai_api_team_plan_monthly_usage",
+    STRIPE_TEAM_YEARLY_FLAT_LOOKUP_KEY: "keywords_ai_api_team_plan_yearly_flat",
+    STRIPE_TEAM_YEARLY_USAGE_LOOKUP_KEY: "keywords_ai_api_team_plan_yearly_usage",
+    SANITY_CHECK: "Sanity check passed for test branch"
 }
