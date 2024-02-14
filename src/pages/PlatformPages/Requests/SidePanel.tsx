d747b0848b062d5d435146ca5a6fb8afec3fc791
failury <failury29@gmail.com>
2024-02-14 00:01:50 -0500

    Refactor Evaluation component in SidePanel.tsx

--- src/pages/PlatformPages/Requests/SidePanel.tsx
+++ src/pages/PlatformPages/Requests/SidePanel.tsx
@@ -402,7 +402,6 @@ const Evaluation = ({
     const planLevel = state.organization?.organization_subscription.plan_level;
     return planLevel < 2;
   });
-  groundnessScore *= 100;
   groundnessScore = parseFloat(groundnessScore.toFixed(2));
 
   return (
@@ -410,47 +409,58 @@ const Evaluation = ({
       aria-label="frame 1974"
       className="flex-col  px-lg py-sm items-start gap-xs self-stretch"
     >
-      {groundnessScore > 0 && (
-        <div className="flex h-[24px] justify-between items-center self-stretch">
-          <Tooltip
-            side="right"
-            sideOffset={8}
-            delayDuration={1}
-            skipDelayDuration={1}
-            content={
-              <>
-                <span className="text-gray-4 caption">
-                  Failure threshold = 85%
-                </span>
-              </>
-            }
-          >
-            <div className="flex items-center gap-xxs text-sm-md text-gray-5">
-              Groundedness
-              <Info />
-            </div>
-          </Tooltip>
-          <div className="flex items-center gap-xxxs">
-            {isFreeUser ? (
+      <div className="flex h-[24px] justify-between items-center self-stretch">
+        <Tooltip
+          side="right"
+          sideOffset={8}
+          delayDuration={1}
+          skipDelayDuration={1}
+          content={
+            <>
+              <span className="text-gray-4 caption">
+                Failure threshold = 0.85
+              </span>
+            </>
+          }
+        >
+          <div className="flex items-center gap-xxs text-sm-md text-gray-5">
+            Groundedness
+            <Info />
+          </div>
+        </Tooltip>
+        <div className="flex items-center gap-xxxs">
+          {isFreeUser ? (
+            <Tag
+              text={"Upgrade"}
+              backgroundColor="bg-primary/10"
+              textColor="text-primary"
+              border=""
+            />
+          ) : groundnessScore > 0 ? (
+            <div className="flex items-center gap-xxxs">
               <Tag
-                text={"Upgrade"}
-                backgroundColor="bg-primary/10"
-                textColor="text-primary"
-                border=""
-              />
-            ) : (
-              <Tag
-                text={`${groundnessScore}% Grounded`}
+                text={`${groundnessScore}`}
                 backgroundColor={
                   groundnessScore >= 85 ? "bg-green/10" : "bg-red/10"
                 }
                 textColor={groundnessScore >= 85 ? "text-green" : "text-red"}
                 border=""
               />
-            )}
-          </div>
+              <Tag
+                text={` Grounded`}
+                backgroundColor={
+                  groundnessScore >= 85 ? "bg-green/10" : "bg-red/10"
+                }
+                textColor={groundnessScore >= 85 ? "text-green" : "text-red"}
+                border=""
+              />
+            </div>
+          ) : (
+            <p className="text-sm-regular text-gray-4">N/A</p>
+          )}
         </div>
-      )}
+      </div>
+
       <div className="flex h-[24px] justify-between items-center self-stretch">
         <div className="flex items-center gap-xxs text-sm-md text-gray-5">
           Sentiment
@@ -464,8 +474,10 @@ const Evaluation = ({
               textColor="text-primary"
               border=""
             />
+          ) : sentimentScore ? (
+            <SentimentTag sentiment_score={sentimentScore} />
           ) : (
-            <SentimentTag showScore={false} sentiment_score={sentimentScore} />
+            <p className="text-sm-regular text-gray-4">N/A</p>
           )}
         </div>
       </div>
