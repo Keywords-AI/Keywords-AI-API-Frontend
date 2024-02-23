6ea6cecd1feab9cbacd6776117ce510bea282b2e
failury <failury29@gmail.com>
2024-02-23 18:00:13 -0500

    Refactor addMissingDate function to use Date object for comparison

--- src/utilities/objectProcessing.tsx
+++ src/utilities/objectProcessing.tsx
@@ -284,7 +284,7 @@ export const addMissingDate = (
     for (let hour = 0; hour < 24; hour++) {
       const hourString = formatTimeUnit(hour);
       const found = data.find((d) => {
-        return d.date_group.split(":")[0] === hour.toString();
+        return new Date(d.date_group).getHours() === hour;
       });
       newDataArray.push(
         found ? { ...found } : { date_group: hourString, ...defaultFields }
