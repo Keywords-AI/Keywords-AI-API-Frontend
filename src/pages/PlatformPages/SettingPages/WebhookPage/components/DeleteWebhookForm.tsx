f2769f177b5d00c34018a8d2500a8f25fc317ab8
Raymond Huang <1415176717@qq.com>
2024-03-11 01:47:05 -0700

    update

---
+++ src/pages/PlatformPages/SettingPages/WebhookPage/components/DeleteWebhookForm.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Button } from 'src/components/Buttons'
+import { formProps } from 'src/types/form'
+import { useTypedDispatch, useTypedSelector } from 'src/store/store'
+import { deleteWebhook } from 'src/store/actions'
+
+export function DeleteWebhookForm({
+    handleClose = () => {},
+}: formProps) {
+    const dispatch = useTypedDispatch()
+    const deleteingWebhook = useTypedSelector((state) => state.webhook.deletingWebhook)
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (deleteingWebhook) {
+            dispatch(deleteWebhook(deleteingWebhook))
+        }
+        handleClose();
+    }
+  return (
+    <form
+        className={"flex-col gap-sm self-stretch relative"}
+        onSubmit={handleSubmit}
+      >
+        <div className="flex-row justify-end self-stretch">
+          <div className="flex-row gap-xs">
+            <Button
+              variant="r4-black"
+              bgColor="transparent"
+              text={"Cancel"}
+              type="button"
+              onClick={handleClose}
+            />
+            <Button variant="r4-red" text={"Delete"} />
+          </div>
+        </div>
+      </form>
+  )
+}
