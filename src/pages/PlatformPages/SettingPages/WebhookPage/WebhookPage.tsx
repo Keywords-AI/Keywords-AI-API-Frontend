bc987a18caffdd73e2f4065c37c693bb5b226d9b
failury <failury29@gmail.com>
2024-03-13 23:58:42 -0400

    Add TitleStaticSubheading component to WebhookPage

--- src/pages/PlatformPages/SettingPages/WebhookPage/WebhookPage.tsx
+++ src/pages/PlatformPages/SettingPages/WebhookPage/WebhookPage.tsx
@@ -9,6 +9,7 @@ import {
   DeleteWebhookForm,
 } from "./components";
 import { setDeletingWebhook } from "src/store/actions";
+import { TitleStaticSubheading } from "src/components/Titles";
 
 export function WebhookPage() {
   const [open, setOpen] = useState(false);
@@ -25,34 +26,51 @@ export function WebhookPage() {
       title="Webhooks"
       subtitle="Subscribe to events in your organization."
     >
-      <WebhookTable />
-      <Modal
-        trigger={<Button variant="r4-primary" text="Create Webhook" />}
-        open={open}
-        setOpen={setOpen}
-        title="Create Webhook"
-        subtitle="Define a new webhook for Keywords AI to send the request log data to."
-      >
-        <CreateWebhookForm
-          handleClose={(e: any) => {
-            setOpen(false);
-          }}
+      <div className="flex-col gap-sm items-start">
+        <TitleStaticSubheading
+          title={"Manage webhooks"}
+          subtitle={
+            <span>
+              Read the{" "}
+              <a
+                className="text-primary cursor-pointer"
+                href="https://docs.keywordsai.co/"
+              >
+                documentation
+              </a>{" "}
+              on using webhooks.
+            </span>
+          }
         />
-      </Modal>
-      <Modal
-        open={Boolean(deletingWebhook)}
-        setOpen={() => {
-          dispatch(setDeletingWebhook(null));
-        }}
-        title="Delete Webhook"
-        subtitle={`Are you sure you want to delete the webhook "${deletingWebhook?.name}"?`}
-      >
-        <DeleteWebhookForm
-          handleClose={(e: any) => {
+        <WebhookTable />
+        <Modal
+          trigger={<Button variant="r4-primary" text="Create new webhook" />}
+          open={open}
+          setOpen={setOpen}
+          title="Create Webhook"
+          subtitle="Define a new webhook for Keywords AI to send the request log data to."
+        >
+          <CreateWebhookForm
+            handleClose={(e: any) => {
+              setOpen(false);
+            }}
+          />
+        </Modal>
+        <Modal
+          open={Boolean(deletingWebhook)}
+          setOpen={() => {
             dispatch(setDeletingWebhook(null));
           }}
-        />
-      </Modal>
+          title="Delete Webhook"
+          subtitle={`Are you sure you want to delete the webhook "${deletingWebhook?.name}"?`}
+        >
+          <DeleteWebhookForm
+            handleClose={(e: any) => {
+              dispatch(setDeletingWebhook(null));
+            }}
+          />
+        </Modal>
+      </div>
     </PageContent>
   );
 }
