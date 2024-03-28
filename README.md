39d46904d07169a7cfb2e5189605067754034432
Raymond Huang <1415176717@qq.com>
2024-03-27 19:08:43 -0700

    env

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Keywords-AI-API-Frontend
-Testing Ellipsis
+
+# Push to a server
+```
+# Install firebase
+npm install -g firebase
+
+# Login to team@keywordsai.co
+firebase login
+
+# Deploy
+source ./deploy.sh
+```
+
--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,5 @@
 import { envVars } from "./branch_env";
 
-envVars
 // Fallbacks are all production settings, @Raymond if you want to change or accidently changed them.
 // const envVars = undefined;
 export const FETCH_ENDPOINT =
